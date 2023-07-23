db293425702b6e0c4d7a441846dd7946b0a0bf4a
Viktor Zbodulja <viktorzbodulja@gmail.com>
2023-07-23 21:26:36 +0200

    My Work section edited

--- src/components/Aplication.jsx
+++ src/components/Aplication.jsx
@@ -13,17 +13,20 @@ function Aplication({appData}) {
         <div className='app_container'>
           <img src={appData.cover} alt="appData.name" />
           <div className='app_description'>
-            <div>
+            <div className='text_container'>
               <div>{appData.description}</div>
               <div>{appData.aditional1}</div>
               <div>{appData.aditional2}</div>
             </div>
             <div className='stack'>
-            {appData.stack.map((s, index) => (
-                <div className='stack_img' key={index}>
-                  <img className='stack_svg' src={s} />
-                </div>
-              ))}
+              <div className='stack_text'>Stack:</div>
+              <div className='stack_container'>
+                {appData.stack.map((s, index) => (
+                  <div className='stack_img' key={index}>
+                    <img className='stack_svg' src={s} />
+                  </div>
+                ))}
+              </div>
             </div>
             <div className='app_buttons'>
               <a href={appData.gitHub} target="_blank"><button>GitHub</button></a>
